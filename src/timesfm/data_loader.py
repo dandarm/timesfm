8b3632433326926ed4b54bd38756077ee53a4a03
dandarm <daniele.darmiento@gmail.com>
2025-01-22 11:44:29 +0100

    Avoid OOM with tensorflow
    tf.config.experimental.set_memory_growth(gpu, True)

--- src/timesfm/data_loader.py
+++ src/timesfm/data_loader.py
@@ -23,6 +23,15 @@ from sklearn.preprocessing import StandardScaler
 import tensorflow as tf
 from . import time_features
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            print('=== setting gpu memory growth to True for gpu: ', gpu)
+            tf.config.experimental.set_memory_growth(gpu, True) 
+    except RuntimeError as e:
+        print(e)
+
 
 class TimeSeriesdata(object):
   """Data loader class."""
